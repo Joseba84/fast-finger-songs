eb38855aa56096e20e20b63eae23a21d036331ff
Joseba <joseba.bello@gmail.com>
2017-09-29 08:08:28 +0200

    change text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <p>Para aquellos que padecen el sindrome del "Fast Finger".</p>
-    <p> Y no pueden agunatar más de 30 segundos escuchando una canción... siempre con el dedito en el botón de siguiente...</p>
+    <p> Y no pueden aguantar más de 30 segundos escuchando una canción... siempre con el dedito en el botón de siguiente...</p>
     <router-link to="/songs"><a>Este va a ser tu único click!</a></router-link>
   </section>
 </template>
